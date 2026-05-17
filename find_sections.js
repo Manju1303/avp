1ce32ad0e833578e0c9c798ea60fb27b07dc8cd7
Manju1303 <arockiamedicalcentre@gmail.com>
2026-05-17 23:18:28 +0530

    Cleanup: Remove temporary SEO scripts

--- find_sections.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('public/index.html', 'utf8');
-html.split('\n').forEach((line, idx) => {
-  if (line.includes('<section') || line.includes('id="') || line.includes('testimonials')) {
-    console.log((idx+1) + ': ' + line.trim());
-  }
-});
